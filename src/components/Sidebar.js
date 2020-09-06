87eac98c95eb00db0a5b794cdea512b20f791c96
mtodorov95 <mtodorov95@gmail.com>
2020-09-06 10:00:10 +0300

    sidebar header/search

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar src="https://media.wired.com/photos/5cdefb92b86e041493d389df/191:100/w_1280,c_limit/Culture-Grumpy-Cat-487386121.jpg" />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchIcon />
+          <input placeholder="Search or start new chat" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
