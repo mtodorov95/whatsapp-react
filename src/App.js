65d05a7cb4d5bea2d789f793487e0997057d9cfa
mtodorov95 <mtodorov95@gmail.com>
2020-09-07 10:31:57 +0300

    get realtime messages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Pusher from "pusher-js";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  // Get all messages on load
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  // Get new message on change
   useEffect(() => {
     const pusher = new Pusher("79443249bbc2e31b144d", {
       cluster: "eu",
     });
 
     const channel = pusher.subscribe("messages");
-    channel.bind("inserted", (data) => {
-      alert(JSON.stringify(data));
+    channel.bind("inserted", (newMessage) => {
+      setMessages([...messages, newMessage]);
     });
-  }, []);
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
 
   return (
     <div className="app">
