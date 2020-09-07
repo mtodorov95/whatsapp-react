e8b70a497a3ef0d78d1577cd74ea06f5baa4466d
mtodorov95 <mtodorov95@gmail.com>
2020-09-07 10:14:17 +0300

    pusher

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher("79443249bbc2e31b144d", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (data) => {
+      alert(JSON.stringify(data));
+    });
+  }, []);
+
   return (
     <div className="app">
       <div className="app__body">
